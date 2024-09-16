b6fd22119168db8d06d33af06d4114c5c9a50e06
HobbitDur <ludovic791@gmail.com>
2024-09-16 11:22:51 +0200

    Working

--- kernel/kernelsection.py
+++ kernel/kernelsection.py
@@ -2,9 +2,11 @@ from gamedata import GameData
 
 
 class KernelSection():
-    def __init__(self, game_data: GameData, data=bytearray(), id:int=0, offset:int=0):
-        self.__data = data
-        self.__size = len(self.__data)
-        self.__id = id
-        self.__game_data = game_data
-        self.__offset = 0
+    def __init__(self, game_data: GameData, data_hex:bytearray, id:int, own_offset:int):
+        self._data_hex = data_hex
+        self._size = len(self._data_hex)
+        self.id = id
+        self._game_data = game_data
+        self.own_offset = own_offset
+    def get_size(self):
+        return self._size
